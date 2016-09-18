1b6a7db7d5ff832624cd8f111159ec1ea2c6a5b2
ding288 <123929187@qq.com>
2016-09-18 22:49:25 +0800

    修改
    修改

--- struts2.5-hibernate5.0/src/main/java/com/di/dao/ModuleDao.java
+++
@@ -1,7 +0,0 @@
-package com.di.dao;
-
-import com.di.model.Module;
-
-public interface ModuleDao extends AbstractDao<Module>{
-
-}
--- struts2.5-hibernate5.0/src/main/java/com/di/service/ModuleService.java
+++
@@ -1,7 +0,0 @@
-package com.di.service;
-
-import com.di.model.Module;
-
-public interface ModuleService extends AbstractService<Module>{
-
-}
